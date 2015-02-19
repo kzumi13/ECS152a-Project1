b739beedd8ba17ef86c00f3620461c9ecae3be42
unknown <kzumi13@gmail.com>
2015-02-18 22:28:35 -0800

    finished 3.1.2.6 working on 3.3

--- main.py
+++ main.py
@@ -1,48 +1,77 @@
-from queue import *
-import Random
+import queue
+import random
+import math
 
-#3.3 Arrival process the event   
-def procArrEvent(my_event, cur_time, q_len):
-    my_event
-    #set the current time to the event time
-    #process the arrival event
+#3.6 Generating Time Intervals in Negative Exponential Distribution
+def nedTime(rate):
+    u = random.random()
+    return ((-1/rate) * math.log(1-u))
+#ENDOF 3.6
     
 class Event():
     #event time
     #type
     #next event
     #prev event
-    def __init__(self, event_time, event_type, event_n_event, event_p_event):
+    def __init__(self, event_time, event_type, service_t): #event_n_event, event_p_event):
         self.time = event_time
         self.type = event_type #0 indicates arrival, 1 indicates departure
-        self.n_event = event_n_event
-        self.p_event = event_p_event
-    
+        self.service_t = service_t #service time of the event
+        #self.n_event = event_n_event
+        #self.p_event = event_p_event
 
 def main():
 #3.2 INITIALIZATION
+    cur_time = 0 #the current time
     MAXBUFFER = 10  #MAXBUFFER is abitrary.... apparently
-    fifoQ = Queue(maxsize= MAXBUFFER) #initialize the queue
+    fifoQ = queue.Queue(maxsize= MAXBUFFER) #initialize the queue
+     
     
-
     #initialize the GEL
     #Python list will act as our linked list
     gel = []
 
-    s_rate = 0 #service rate
-    a_rate = 0 #arrival rate
+    a_rate = .5 #arrival rate
+    d_rate = .5 #service rate
 
-    init_event = Event(0,0,None,None) #first event = arrival
-    fifoQ.put(init_event)
+    init_event = Event(cur_time + nedTime(a_rate),0,nedTime(d_rate)) #first event = arrival
+    gel.append(init_event)
+#ENDOF 3.2
     
-    #for i in range(0, 100000):
+    for i in range(0, 10):
         #get the first event from the GEL
-        #if the event is an arrival, the process-arrival-event
-        #Otherwise it must be a departure event and hence process-service-completion
+        cur_event = gel[0]
 
+#3.3    PROCESS THE ARRIVAL EVENT
+        if cur_event.type == 0:
+        #set the event time to the current event time
+            cur_event.time = cur_time
+            
+            #1)generate the next arrival event
+            ar_time = cur_time + nedTime(a_rate)
+            #2)create a new packet with serivce <--------------------------- THIS ONE STILL NEEDS WORK!?!?!? I DONT UNDERSTAND!!!!
+            service_t = nedTime(d_rate)
+            #3)create a new arrival event
+            new_event = Event(ar_time, 0, service_t)
+            #4)Insert the event into the list
+            gel.append(new_event)
+            gel.sort(key=lambda x: x.event_time) #sort based on the event time
+
+        #process the arrival event
+            #a)if server is free, schedule for transmission
+            if fifoQ.qsize() == 0:
+                
+#ENDOF 3.3
+                
     #output statistics
-    if fifoQ.empty() == False:
-        print(fifoQ.get())
-    print(gel)
+    #if fifoQ.empty() == False:
+    #    print(fifoQ.get())
+    #print(gel)
+
+    for events in gel:
+        print(events.service_t)
+    
+    mylist = [0,1,2,3,4,5]
+    print(mylist[1]);
 
 main()
